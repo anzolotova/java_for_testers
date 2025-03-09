49df6e8591916296311690927f78ab0073b8318a
Anastasiya <a.zolotova.smm@gmail.com>
2025-03-09 21:09:03 +0300

    Homework 6

--- addressbook-web-tests/src/test/java/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/tests/TestBase.java
@@ -12,7 +12,7 @@ public class TestBase {
         if (app == null) {
             app = new ApplicationManager();
         }
-        app.init("firefox");
+        app.init(System.getProperty("browser", "chrome"));
     }
 
 }
